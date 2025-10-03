3f7af79842ca67f2ac78bba743083d30a1ea6728
Cristhofer Urrutia <cristhofer.urrutia@ucr.ac.cr>
2025-10-03 01:18:49 -0600

    feat: se agregan funciones var cvar

--- src/data/main.py
+++ src/data/main.py
@@ -6,6 +6,14 @@ bono_normal = inferencia(["BND"], "2023-01-01", "2025-01-01", 0.05)
 bono_normal.graficar_rendimientos("BND")
 print(bono_normal.normalidad("BND", True))
 
+print(bono_normal.rendimientos_diarios())
+
 # Resultado 2 Sprint 2: VaR y CVaR en bonos a plazos
-var_cvar_bnd = var_cvar(["BND"], "2023-01-01", "2025-01-01")
+var_cvar_bnd = var_cvar(["BND"], "2000-01-01", "2025-01-01")
 print(var_cvar_bnd.var_cvar_normal(["BND"], True))
+
+# Resultado 3 Sprint 3-4:
+
+var_cvar_bnd.cola_izquierda(["BND"], True, 0.1)
+var_cvar_bnd.pareto_generalizada("BND", 0.1, True)
+print(var_cvar_bnd.var_cvar_gpd("BND", 0.95, 0.1))
--- src/data/var_cvar.py
+++ src/data/var_cvar.py
@@ -1,5 +1,10 @@
 from inferencia import inferencia
 from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import genpareto
+from scipy.stats import kstest
 
 
 class var_cvar(inferencia):
@@ -34,3 +39,106 @@ class var_cvar(inferencia):
             return {"VaR": var, "CVaR": cvar}
 
         return {"VaR": var, "CVaR": cvar}
+
+    def cola_izquierda(self, empresa: str, graficar=False, q=0.05):
+        umbral = np.quantile(self.rendimientos_diarios()[empresa], q)
+
+        cola = self.rendimientos_diarios()[
+            self.rendimientos_diarios()[empresa] <= umbral
+        ]
+
+        if graficar:
+            plt.figure(figsize=(10, 5))
+            sns.histplot(cola, bins=50, kde=False, color="navy", alpha=0.7)
+
+            plt.axvline(
+                umbral,
+                color="black",
+                linestyle="--",
+                label=f"Umbral empírico: {umbral:.4f}",
+                alpha=1,
+            )
+
+            plt.title(f"Cola izquierda de {empresa} (q = {q})")
+            plt.xlabel("Rendimientos")
+            plt.ylabel("Frecuencia")
+            plt.legend()
+            plt.grid(True)
+            plt.tight_layout()
+            plt.show()
+
+            return cola
+
+        else:
+            return cola
+
+    def pareto_generalizada(self, empresa, q: float, graficar=False):
+        """
+        Obtiene la distribución generalizada de pareto de
+        una distribución basado en POT.
+            q (float): percentil.
+
+        Returns:
+            parámetros (dict): parámetros de la gpd.
+        """
+
+        threshold = np.quantile(self.rendimientos_diarios()[empresa], q)
+
+        excedentes = threshold - self.cola_izquierda(empresa, False, q)
+
+        xi, loc, sigma = genpareto.fit(excedentes, floc=0)
+
+        ks_stat, p_val = kstest(excedentes, "genpareto", args=(xi, loc, sigma))
+
+        if graficar:
+            x = np.linspace(0, excedentes.max(), 1000)
+            fitted_pdf = genpareto.pdf(x, xi, loc=0, scale=sigma)
+
+            plt.figure(figsize=(10, 5))
+            sns.histplot(
+                excedentes,
+                bins=100,
+                stat="density",
+                alpha=0.7,
+                label="Excedentes",
+                color="navy",
+            )
+
+            plt.plot(x, fitted_pdf, "r-", lw=2, label="GPD ajustada")
+            plt.title(f"Ajuste GPD a excedentes de {empresa}")
+            plt.xlabel("Excedente (threshold - rendimientos menores)")
+            plt.ylabel("Frecuencia")
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+
+            return {"xi": xi, "sigma": sigma, "p valor": p_val, "threshold": threshold}
+
+        elif graficar is not True:
+            return {"xi": xi, "sigma": sigma, "p valor": p_val, "threshold": threshold}
+
+    def var_cvar_gpd(self, empresa, alpha: float, q: float):
+        """
+        Obtiene el var y cvar de los rendimientos dado un nivel de confianza
+        Args:
+            q (float): percentil.
+            alpha (float): nivel de confianza
+
+        Returns:
+            var, cvar (float): var y cvar de los rendimientos
+        """
+        x = self.rendimientos_diarios()[empresa].astype(float).to_numpy()
+
+        u = np.quantile(x, q)
+
+        dic_gpd = self.pareto_generalizada(empresa, q)
+
+        xi, sigma, threshold = dic_gpd["xi"], dic_gpd["sigma"], dic_gpd["threshold"]
+
+        pu = float(np.mean(x <= u))
+
+        VaR = threshold - (sigma / xi) * ((((1 - alpha) / pu) ** -xi) - 1)
+
+        CVaR = VaR - (sigma - xi * (VaR - threshold)) / (1 - xi)
+
+        return VaR, CVaR, threshold, xi
