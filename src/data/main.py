3a84756b422fbcb81c1d7f4d5fb9660d032a0f6d
Cristhofer Urrutia <cristhofer.urrutia@ucr.ac.cr>
2025-10-17 14:20:59 -0600

    feat: objetivos sprint

--- src/data/main.py
+++ src/data/main.py
@@ -9,10 +9,14 @@ print(bono_normal.normalidad("BND", True))
 print(bono_normal.rendimientos_diarios())
 
 # Resultado 2 Sprint 2: VaR y CVaR en bonos a plazos
-var_cvar_bnd = var_cvar(["BND"], "2000-01-01", "2025-01-01")
+var_cvar_bnd = var_cvar(["BND"], "2010-01-01", "2025-01-01")
 print(var_cvar_bnd.var_cvar_normal(["BND"], True))
 
-# Resultado 3 Sprint 3-4:
+# Resultado 3 Sprint 3-4-5:
+
 var_cvar_bnd.cola_izquierda(["BND"], True, 0.1)
 var_cvar_bnd.pareto_generalizada("BND", 0.1, True)
-print(var_cvar_bnd.var_cvar_gpd("BND", 0.95, 0.1))
+
+
+# print(var_cvar_bnd.var_cvar_gpd("BND", 0.95, 0.1))
+print(var_cvar_bnd.pareto_generalizada("BND", 0.1))
